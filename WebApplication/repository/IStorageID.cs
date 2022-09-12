5fddb94a2d338528ada03d85665ad9f452bc9412
Oleg Aguzarov <olegaguzarov62rzn@gmail.com>
2022-09-13 02:48:05 +0300

    .
    Малозначимое поверхностное что-то

---
+++ WebApplication/repository/IStorageID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+using WebApplication.Domains;
+
+namespace WebApplication.repository
+{
+    public interface IStorageID
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ WebApplication/repository/Storages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Domains;
+using static System.Net.Mime.MediaTypeNames;
+using WebApplication.repository;
+
+namespace WebApplication.repository
+{
+    public class Storages
+    {
+        public static Storage<Human> HumanStorage { get; } = new();
+    }
+}
+
+
